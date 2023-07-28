5edc52fd3baa09f30273a447fce9a2e531e40ccb
Gururaj-Math <gururajmath3@gmail.com>
2023-07-28 13:04:49 +0530

    tailwind add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import Card from "./components/Card";
+import banner from "./assets/banner.png"
+
 function App() {
   return (
-    <div>
+    <div class="conatiner m-auto font-custom">
       <header>
-        <h1>Clothing Shop</h1>
+        <h1 class="text-4xl">Clothing Shop</h1>
       </header>
-      <img src="https://i.postimg.cc/zvLxB4vb/SHOP-NOW.png"></img>
-
+      <img class="rounded-xl w-full" src={banner}></img>
+      <div class="grid grid-cols-4 gap-10 my-4">
       <Card />
+      <Card />
+      <Card />
+      </div>
+
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,10 @@
 
 const Card = () => {
   return (
-    <div>
+    <div class="bg-white p-4 text-center rounded-md shadow-sm shadow-offset">
       <h2>Card Component</h2>
       <p>this is Card Component</p>
+      <button class="btn">Click</button>
     </div>
   )
 }
